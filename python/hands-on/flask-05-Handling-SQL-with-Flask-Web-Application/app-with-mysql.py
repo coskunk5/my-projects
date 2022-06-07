336eadb1c3cb7e6800ebf591a00f3d29233a2aa6
Coskun Kubuz <coskun.kubuz@viking-intl.com>
2022-06-07 22:18:43 +0300

    flask-05-Handling-SQL-with-Flask-Web-Application

--- python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -6,9 +6,9 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'cXXXXXrds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'coskun.cu68usmevvr4.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'databasename'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()

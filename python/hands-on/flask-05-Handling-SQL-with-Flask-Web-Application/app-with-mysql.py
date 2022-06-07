7ead85dd716764451874171bd509dd915790cbe2
Coskun Kubuz <coskun.kubuz@viking-intl.com>
2022-06-07 22:21:02 +0300

    flask-05-Handling-SQL-with-Flask-Web-Application

--- python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_HOST'] = 'coskun.cu68usmevvr4.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
-app.config['MYSQL_DATABASE_DB'] = 'databasename'
+app.config['MYSQL_DATABASE_DB'] = 'clarusway'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()
 mysql.init_app(app)
